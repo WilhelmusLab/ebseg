bddb15b017dcb164153e9c4317914a76c9a17982
John Gerrard Holland <john_holland1@brown.edu>
2024-08-16 11:00:54 -0400

    update template script to remove excess equals signs

--- workflow/cylc/sampled-examples/util/template.py
+++ workflow/cylc/sampled-examples/util/template.py
@@ -16,7 +16,7 @@ def main(datafile: pathlib.Path, index_col: str, index_val: str):
     df = pandas.read_csv(datafile, index_col=index_col)
     r = df.loc[index_val]
     print(
-        f"""--icp {r.startdate} --fcp {r.enddate} --set=BBOX="{r.left_x},{r.lower_y},{r.right_x},{r.top_y}" --set=LOCATION="'{r.location}'" """
+        f"""--icp {r.startdate} --fcp {r.enddate} --set BBOX="{r.left_x},{r.lower_y},{r.right_x},{r.top_y}" --set "LOCATION='{r.location}'" """
     )
 
 
