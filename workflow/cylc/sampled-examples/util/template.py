0bb7d496fbf55cb918e5ab52b57b7fc7184e6400
John Gerrard Holland <john_holland1@brown.edu>
2024-09-04 15:50:31 +0000

    make template into a separate testable function

--- workflow/cylc/sampled-examples/util/template.py
+++ workflow/cylc/sampled-examples/util/template.py
@@ -15,9 +15,33 @@ import typer
 def main(datafile: pathlib.Path, index_col: str, index_val: str):
     df = pandas.read_csv(datafile, index_col=index_col)
     r = df.loc[index_val]
-    print(
-        f"""--icp {r.startdate} --fcp {r.enddate} --set BBOX="{r.left_x},{r.lower_y},{r.right_x},{r.top_y}" --set "LOCATION='{r.location}'" """
+    print(_template(r))
+
+
+def _template(r: pandas.DataFrame):
+    """
+    Examples:
+        >>> import io
+        >>> df = pandas.read_csv(io.StringIO(
+        ... '''fullname,location,size,center_lat,center_lon,top_left_lat,top_left_lon,lower_right_lat,lower_right_lon,left_x,right_x,lower_y,top_y,startdate,enddate
+        ... 998-beaufort_sea,beaufort_sea,1200km,,,-152.9892,67.6236,-114.8220,78.9424,-2334051,-1127689,-414418,757892,2006-05-04,2006-05-06
+        ... 999-beaufort_sea,beaufort_sea,1200km,,,-152.9892,67.6236,-114.8220,78.9424,-2334051,-1127689,-414418,757892,2008-07-13,2008-07-15
+        ... '''), 
+        ... index_col="fullname")
+        >>> _template(df.loc["998-beaufort_sea"])
+        '--icp 2006-05-04 --fcp 2006-05-06 --set BBOX="-2334051,-414418,-1127689,757892" --set LOCATION="beaufort_sea" '
+        >>> _template(df.loc["999-beaufort_sea"])
+        '--icp 2008-07-13 --fcp 2008-07-15 --set BBOX="-2334051,-414418,-1127689,757892" --set LOCATION="beaufort_sea" '
+
+    """
+
+    s = (
+        f"""--icp {r.startdate} """
+        f"""--fcp {r.enddate} """
+        f"""--set BBOX="{r.left_x},{r.lower_y},{r.right_x},{r.top_y}" """
+        f"""--set LOCATION="{r.location}" """
     )
+    return s
 
 
 if __name__ == "__main__":
