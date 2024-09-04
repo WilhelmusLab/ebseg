9c66729f22061f7b174ca0767d09884e3c9bdfa8
John Gerrard Holland <john_holland1@brown.edu>
2024-09-04 17:47:18 -0400

    Update workflow/cylc/sampled-examples/util/template.py
    Co-authored-by: Carlos Paniagua <cpaniaguam@gmail.com>

--- workflow/cylc/sampled-examples/util/template.py
+++ workflow/cylc/sampled-examples/util/template.py
@@ -24,7 +24,7 @@ def _template(df, row_index):
     """
     Examples:
 
-        >>> import io
+        >>> import io, pandas
         >>> csv = '''id,location,center_lat,center_lon,top_left_lat,top_left_lon,lower_right_lat,lower_right_lon,left_x,right_x,lower_y,top_y,startdate,enddate
         ... beaufort_sea,beaufort_sea,75,-135,67.22298,-152.46426,79.32881,-94.68433,-2383879,-883879,-750000,750000,2020-09-05,2020-09-08
         ... hudson_bay,hudson_bay,60,-83,59.65687,-101.24295,57.54266,-66.04186,-2795941,-1295941,-3368686,-1868686,2020-09-06,2020-09-09
