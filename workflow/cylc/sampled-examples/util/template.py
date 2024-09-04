b2ee2ac44c47916e6aae3d4476e96a2cae54ea94
John Gerrard Holland <john_holland1@brown.edu>
2024-09-04 21:47:41 +0000

    simplify examples by removing unused fields

--- workflow/cylc/sampled-examples/util/template.py
+++ workflow/cylc/sampled-examples/util/template.py
@@ -25,17 +25,17 @@ def _template(df, row_index):
     Examples:
 
         >>> import io
-        >>> csv = '''id,location,center_lat,center_lon,top_left_lat,top_left_lon,lower_right_lat,lower_right_lon,left_x,right_x,lower_y,top_y,startdate,enddate
-        ... beaufort_sea,beaufort_sea,75,-135,67.22298,-152.46426,79.32881,-94.68433,-2383879,-883879,-750000,750000,2020-09-05,2020-09-08
-        ... hudson_bay,hudson_bay,60,-83,59.65687,-101.24295,57.54266,-66.04186,-2795941,-1295941,-3368686,-1868686,2020-09-06,2020-09-09
+        >>> csv = '''id,location,left_x,right_x,lower_y,top_y,startdate,enddate
+        ... beaufort-sea-0,beaufort-sea,-2383879,-883879,-750000,750000,2020-09-05,2020-09-08
+        ... hudson-bay-0,hudson-bay,-2795941,-1295941,-3368686,-1868686,2020-09-06,2020-09-09
         ... '''
         >>> df = pandas.read_csv(io.StringIO(csv), index_col="id")
 
-        >>> print(_template(df, row_index="beaufort_sea"))
-        --icp 2020-09-05 --fcp 2020-09-08 --set BBOX="-2383879,-750000,-883879,750000" --set LOCATION="beaufort_sea"
+        >>> print(_template(df, row_index="beaufort-sea-0"))
+        --icp 2020-09-05 --fcp 2020-09-08 --set BBOX="-2383879,-750000,-883879,750000" --set LOCATION="beaufort-sea"
 
-        >>> print(_template(df, row_index="hudson_bay"))
-        --icp 2020-09-06 --fcp 2020-09-09 --set BBOX="-2795941,-3368686,-1295941,-1868686" --set LOCATION="hudson_bay"
+        >>> print(_template(df, row_index="hudson-bay-0"))
+        --icp 2020-09-06 --fcp 2020-09-09 --set BBOX="-2795941,-3368686,-1295941,-1868686" --set LOCATION="hudson-bay"
 
     """
     r = df.loc[row_index]
