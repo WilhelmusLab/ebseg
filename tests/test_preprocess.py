6bc9203c068f15f833651a82a22cbf3f24d14fa9
John Gerrard Holland <john_holland1@brown.edu>
2025-01-27 21:36:59 +0000

    test(preprocess): add test throwing an error due to multiple disconnected components having the same label

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,6 +1,14 @@
+import datetime
+import os
 import pytest
 from pathlib import Path
-from ebfloeseg.preprocess import preprocess
+import shutil
+
+import numpy as np
+import rasterio
+import skimage.measure
+
+from ebfloeseg.preprocess import preprocess, preprocess_b 
 
 
 def test_process_exception(tmpdir):
@@ -32,3 +40,43 @@ def test_process_exception(tmpdir):
             erosion_kernel_type,
             erosion_kernel_size,
         )
+
+
+@pytest.fixture
+def process_data_directory(tmp_path, date="2012-08-01", day=214, satellite="terra"):
+    test_dir = os.path.dirname(__file__)
+    shutil.copyfile(Path(f"{test_dir}/input/reproj_land.tiff"), tmp_path / Path("landmask.tiff"))
+    shutil.copyfile(Path(f"{test_dir}/input/tci/tci_{date}_{day}_{satellite}.tiff"), tmp_path / Path("truecolor.tiff"))
+    shutil.copyfile(Path(f"{test_dir}/input/cloud/cloud_{date}_{day}_{satellite}.tiff"), tmp_path / Path("cloud.tiff"))
+    return tmp_path
+
+def test_process_no_duplicated_labels(process_data_directory: Path):
+    
+    preprocess_b(
+        ftci = process_data_directory / "truecolor.tiff",
+        fcloud = process_data_directory / "cloud.tiff",
+        fland = process_data_directory / "landmask.tiff",
+        save_figs = True,
+        save_direc = process_data_directory,
+        fname_prefix="",
+        itmax = 8,
+        itmin = 3,
+        step = -1,
+        erosion_kernel_type = "diamond",
+        erosion_kernel_size = 1,
+        date=datetime.date.fromisoformat("2001-01-01")
+    )
+
+    with rasterio.open(process_data_directory / "final.tif") as dataset:
+        image_array = dataset.read()
+        for l in np.unique(image_array)[1:]:
+            mask = l == image_array
+            _, num = skimage.measure.label(mask, return_num=True)
+            assert num == 1, f"Disconnected component detected for label {l=}"
+
+
+
+
+
+if __name__ == "__main__":
+    pytest.main()
